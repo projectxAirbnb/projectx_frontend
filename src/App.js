7a3b3fc41643c6c35354370d3336f87766d0ffbb
Abd. Rauf Jauhari Izza Mazzidan <raufmazzidan@gmail.com>
2019-11-27 12:56:46 +0700

    add-codebase

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import pages from './pages';
+import { ROUTES } from './configs';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <CssBaseline />
+        <Switch>
+          <Route exact path={ROUTES.HOME()} component={pages.Home} />
+          <Route exact path={ROUTES.DEVELOPER(':name')} component={pages.Developer} />
+          <Route component={pages.Error404} />
+        </Switch>
+      </main>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.element
+};
---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import home from '../pages/Home/reducer';
+
+const rootReducer = combineReducers({
+  home,
+  routing: routerReducer
+});
+
+export default rootReducer;
